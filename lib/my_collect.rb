350582e919b88ba83435dca9d63c7ff877b22daa
dennis wang <dwang0816@gmail.com>
2019-06-12 19:09:19 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,9 @@
 def my_collect(array)
-  i = 0 
-  new_array = []
-  while i < array.length 
-    new array << yield(array[i])
-    i = i + 1 
-  end
-  new_array
-end 
+    i = 0
+    arr = []
+    while i < array.length
+     arr.push yield(array[i])
+      i+=1
+    end
+    arr
+end
